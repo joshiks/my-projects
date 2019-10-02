760d15b972213ad829ff12680280096241b77994
joshiks <56087031+joshiks@users.noreply.github.com>
2019-10-02 22:49:32 +0530

    Add files via upload

---
+++ abc/libs/app.js
@@ -0,0 +1,12 @@
+
+    /*angular.module("myModule", []).controller("myController", function ($scope) {
+        $scope.items = [
+            {name: "Pencil", price: 7, item_type: 'A'},
+            {name: "Pen", price: 10, item_type: 'A'},
+            {name: "Pen drive", price: 1000, item_type: 'B'},
+            {name: "Mobile", price: 12000, item_type: 'B'}
+        ]
+
+
+    });*/
+
